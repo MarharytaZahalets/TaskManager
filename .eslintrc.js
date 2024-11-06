508e528dd7e8dc57213a3d4085888b2c468a2b07
Marharyta Zahalets <mzagalec@gmail.com>
2024-11-06 14:43:55 +0000

    Initial commit. Config ESLint, Prettier, TS

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:@typescript-eslint/recommended', // recommended rules for TS
+    'prettier', // disable ESLint rules that conflict with Prettier
+    'plugin:import/typescript', // TS support for import/export linting
+    'plugin:react-native/all', // recommended rules for RN
+  ],
+  parser: '@typescript-eslint/parser', // TS parser for ESLint to understand TS syntax
+  plugins: [
+    '@typescript-eslint', // additional TS rules
+    'prettier', // enable Prettier to show formatting issues as ESLint errors
+    'unused-imports', // listing for unused imports and variables
+    'react-native', // additional RN rules
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'], // specify files for parser
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // allow resolve types even if they’re not explicitly imported
+        project: '<root>/tsconfig.json', // link to TS config
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // apply rules only for TS files
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'], // prevent variable naming conflicts
+        '@typescript-eslint/no-var-requires': 'off', // allow using require() in TS files
+        'no-shadow': 'off', // disable default ESLint no-shadow rule to avoid conflicts
+        'no-undef': 'off', // disable no-undefined ESLint rule, TS handles this
+        'prettier/prettier': ['error'], // flag Prettier issues as errors
+        '@typescript-eslint/no-unused-vars': 'error', // flag unused variables as errors
+        'no-unused-vars': 'off', // disable ESLint no-unused-vars rule to avoid conflicts
+        'no-console': 'error', // disallow console logs for deploy (for dev change to 'warn')
+        'unused-imports/no-unused-imports': 'error', // auto-removal for unused imports
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_', // ignore vars with this prefix
+            args: 'after-used', // flags unused arguments only if they appear after used ones
+            argsIgnorePattern: '^_', // ignore args with this prefix
+          },
+        ],
+        'no-nested-ternary': 'error', // disallow nested ternary operators for readability
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bracketSameLine: false, // place closing bracket on a new line for multi-line tags
+  bracketSpacing: true, // add spaces inside object brackets
+  singleQuote: true, // use single quotes instead of double
+  printWidth: 90, // set max line length for readability
+  semi: true, // add semicolon at the end of statements
+  jsxSingleQuote: true, // uses single quotes in JSX attributes
+  trailingComma: 'all', // adds trailing commas where valid in ES5 (e.g., objects, arrays)
+  jsxBracketSameLine: false, // put `>` of multi-line JSX tags on a new line
+  arrowParens: 'always', // always include parentheses around arrow function parameters
+  endOfLine: 'lf', // enforce line feed as the end-of-line character
+  tabWidth: 2, // 2-space indentation for compact code
+  useTabs: false, // check that spaces are used instead of tabs
+  proseWrap: 'preserve', // preserve markdown text wrapping
+};
